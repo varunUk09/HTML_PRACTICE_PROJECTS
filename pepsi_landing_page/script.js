d0893c57f8a6326de18fe58c362732fa7ef87b46
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-09-15 19:16:55 +0530

    pepsi landing page design close functionality implemented

--- pepsi_landing_page/script.js
+++ pepsi_landing_page/script.js
@@ -1,18 +1,23 @@
 const navMenuBtn = document.querySelector(".nav_open");
 const nav = document.querySelector(".navigation_top");
-
+const isMenuOpen = false;
 const navCloseBtn = document.querySelector(".close_nav");
 
 navMenuBtn.addEventListener('click',()=>{
 	nav.classList.add("show_nav");
 	navMenuBtn.style.display = "none";
+	isMenuOpen = true;
 });
 
 
 function hideNav(){
-	nav.classList.remove("show_nav");
-	navMenuBtn.style.display = "block";
+	if(isMenuOpen == true){
+		nav.classList.remove("show_nav");
+		navMenuBtn.style.display = "block";
+	}
 }
 navCloseBtn.addEventListener('click',hideNav);
 
-// window.addEventListener('scroll',hideNav);
+window.addEventListener('scroll',hideNav);	
+
+
