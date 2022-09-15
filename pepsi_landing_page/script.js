88c7ffa07881ed1734cfcf09cacfc5f94a46d1fc
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-09-15 18:55:03 +0530

    pepsi landing page design added

---
+++ pepsi_landing_page/script.js
@@ -0,0 +1,18 @@
+const navMenuBtn = document.querySelector(".nav_open");
+const nav = document.querySelector(".navigation_top");
+
+const navCloseBtn = document.querySelector(".close_nav");
+
+navMenuBtn.addEventListener('click',()=>{
+	nav.classList.add("show_nav");
+	navMenuBtn.style.display = "none";
+});
+
+
+function hideNav(){
+	nav.classList.remove("show_nav");
+	navMenuBtn.style.display = "block";
+}
+navCloseBtn.addEventListener('click',hideNav);
+
+window.addEventListener('scroll',hideNav);
