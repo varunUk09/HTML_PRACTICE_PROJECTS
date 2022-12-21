c31e81bfe82e5fc395d567d1f8e681fbf6492ff0
Varun Pandey <varunpandeyuttrakhand+github@gmail.com>
2022-12-21 13:22:28 +0530

    Update script.js
    Try catch added

--- File Downloader V1/script.js
+++ File Downloader V1/script.js
@@ -7,37 +7,51 @@ document.querySelector(".container form").addEventListener("submit", (e) => {
 });
 
 function fetchFile(url) {
-  // fetching file & returning response as blob
-  fetch(url)
-    .then((response) => {
-      // get the file name and extension from the Content-Disposition header, or use a default file name
-      const contentDisposition = response.headers.get("Content-Disposition");
-      let fileName = "";
-      if (contentDisposition) {
-        const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
-        if (fileNameMatch) {
-          fileName = fileNameMatch[1];
+  try {
+    // fetching file & returning response as blob
+    fetch(url)
+      .then((response) => {
+        // get the file name and extension from the Content-Disposition header, or use a default file name
+        const contentDisposition = response.headers.get("Content-Disposition");
+        let fileName = "";
+        if (contentDisposition) {
+          const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
+          if (fileNameMatch) {
+            fileName = fileNameMatch[1];
+          }
         }
-      }
-      if (!fileName) {
-        fileName = "download.bin";
-      }
+        if (!fileName) {
+          fileName = "download.bin";
+        }
 
-      return response.blob().then((file) => {
-        // converting blob file to url
-        const tempUrl = URL.createObjectURL(file);
-        const aTag = document.createElement("a");
-        aTag.href = tempUrl;
-        aTag.download = fileName; // file name
-        document.body.insertAdjacentElement("beforeend", aTag);
-        aTag.click();
-        aTag.remove();
-        URL.revokeObjectURL(tempUrl);
-        DOWNLOAD_BTN.value = "SUBMIT";
+        return response.blob().then((file) => {
+          // converting blob file to url
+          const tempUrl = URL.createObjectURL(file);
+          const aTag = document.createElement("a");
+          aTag.href = tempUrl;
+          aTag.download = fileName; // file name
+          document.body.insertAdjacentElement("beforeend", aTag);
+          aTag.click();
+          aTag.remove();
+          URL.revokeObjectURL(tempUrl);
+          DOWNLOAD_BTN.value = "SUBMIT";
+        });
+      })
+      .catch((error) => {
+        throw error;
       });
-    })
-    .catch((e) => {
-      alert("Failed To Download Please Try Again!!!");
-      DOWNLOAD_BTN.value = "SUBMIT";
-    });
+  } catch (error) {
+    // handle specific errors here
+    if (error instanceof TypeError) {
+      // handle type errors here
+      alert("Invalid URL. Please enter a valid URL.");
+    } else if (error instanceof SyntaxError) {
+      // handle syntax errors here
+      alert("Invalid URL. Please enter a valid URL.");
+    } else {
+      // handle other errors here
+      alert("An error occurred. Please try again.");
+    }
+    DOWNLOAD_BTN.value = "SUBMIT";
+  }
 }
