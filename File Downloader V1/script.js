fc5073445cdcef968f36a875080e5fc2c8b39eae
Varun Pandey <varunpandeyuttrakhand+github@gmail.com>
2022-12-21 13:19:43 +0530

    Update script.js
    Content header

--- File Downloader V1/script.js
+++ File Downloader V1/script.js
@@ -7,21 +7,37 @@ document.querySelector(".container form").addEventListener("submit", (e) => {
 });
 
 function fetchFile(url) {
-    // fecthing file & returning response as blob
-    fetch(url).then(response => response.blob()).then(file => {
+  // fetching file & returning response as blob
+  fetch(url)
+    .then((response) => {
+      // get the file name and extension from the Content-Disposition header, or use a default file name
+      const contentDisposition = response.headers.get("Content-Disposition");
+      let fileName = "";
+      if (contentDisposition) {
+        const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
+        if (fileNameMatch) {
+          fileName = fileNameMatch[1];
+        }
+      }
+      if (!fileName) {
+        fileName = "download.bin";
+      }
+
+      return response.blob().then((file) => {
         // converting blob file to url
         const tempUrl = URL.createObjectURL(file);
         const aTag = document.createElement("a");
         aTag.href = tempUrl;
-        aTag.download = url.split("/").pop(); // file name
+        aTag.download = fileName; // file name
         document.body.insertAdjacentElement("beforeend", aTag);
         aTag.click();
         aTag.remove();
         URL.revokeObjectURL(tempUrl);
         DOWNLOAD_BTN.value = "SUBMIT";
-
-    }).catch(e => {
-        alert("Failed To Download Please Try Again!!!");
-        DOWNLOAD_BTN.value = "SUBMIT";
+      });
+    })
+    .catch((e) => {
+      alert("Failed To Download Please Try Again!!!");
+      DOWNLOAD_BTN.value = "SUBMIT";
     });
 }
