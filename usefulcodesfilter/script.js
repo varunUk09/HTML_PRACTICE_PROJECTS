298e5cfe40d2bb019c88de10ab9e8b251d2bfd6e
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-03-19 15:39:49 +0530

    updated

--- usefulcodesfilter/script.js
+++ usefulcodesfilter/script.js
@@ -152,35 +152,35 @@ for (const key in data) {
         let heading = data[key].tag;
         let code = data[key].code;
         let desc = data[key].desc;
-        document.querySelector(".codes-list").insertAdjacentHTML("beforeend",`<li class="codes-list-item" id="code-snippet-${id}"><h2 class="tagline"></h2><code class="code"></code><pre class="desc"></pre></li>`);
+        document.querySelector(".codes-list").insertAdjacentHTML("beforeend", `<li class="codes-list-item" id="code-snippet-${id}"><h2 class="tagline"></h2><code class="code"></code><pre class="desc"></pre></li>`);
         document.querySelector(`#code-snippet-${id} .tagline`).innerText = heading;
         document.querySelector(`#code-snippet-${id} .code`).innerText = code;
         document.querySelector(`#code-snippet-${id} .desc`).innerText = desc;
-        document.querySelector(`#code-snippet-${id} .desc`).setAttribute("data-tag",key);
+        document.querySelector(`#code-snippet-${id} .desc`).setAttribute("data-tag", key);
         id += 1;
     }
 }
 
-document.querySelector(".search-group").addEventListener("submit",(e)=>{
- e.preventDefault();
+document.querySelector(".search-group").addEventListener("submit", (e) => {
+    e.preventDefault();
 });
 
-document.querySelector(".search-box").addEventListener("input",function(){
-    let query = this.value; 
-     filter(query)
- });
- 
- function filter(query){
-     document.querySelectorAll(".desc").forEach(desc=>{
-    if(desc.getAttribute("data-tag").toUpperCase().indexOf(query.toUpperCase()) == -1){
-        desc.parentElement.classList.add("hide-it");
-    } else {
-        desc.parentElement.classList.remove("hide-it");
-        if(query.trim() !== ""){
-            desc.parentElement.classList.add("filter-match");
+document.querySelector(".search-box").addEventListener("input", function () {
+    let query = this.value;
+    filter(query)
+});
+
+function filter(query) {
+    document.querySelectorAll(".desc").forEach(desc => {
+        if (desc.getAttribute("data-tag").toUpperCase().indexOf(query.toUpperCase()) == -1) {
+            desc.parentElement.classList.add("hide-it");
         } else {
-            desc.parentElement.classList.remove("filter-match");
+            desc.parentElement.classList.remove("hide-it");
+            if (query.trim() !== "") {
+                desc.parentElement.classList.add("filter-match");
+            } else {
+                desc.parentElement.classList.remove("filter-match");
+            }
         }
-    } 
- });
- }
+    });
+}
